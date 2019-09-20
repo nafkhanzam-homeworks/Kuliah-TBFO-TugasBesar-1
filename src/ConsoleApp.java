ca339c1836f326a520206eea42e1b72cf4026c8e
nafkhanzam <nafkhanalzamzami@gmail.com>
2019-09-20 15:31:06 +0700

    deleted Main

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -3,5 +3,7 @@ package tubes;
 import static tubes.Console.*;
 
 public class ConsoleApp {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-package tubes;
-
-import static tubes.Console.*;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
